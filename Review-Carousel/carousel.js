9f8056a41f2f41911df00fbe9eaed622c21d5c85
Formasitchijoh <76694292+Formasitchijoh@users.noreply.github.com>
2023-04-23 01:21:03 +0100

    Day 3: A review Carousel

---
+++ Review-Carousel/carousel.js
@@ -0,0 +1,10 @@
+const review = {
+    image:"",
+    name: "Dawson Tate",
+    profession:" Web Developer",
+    description: " Hello i am enthusiactic web developet that",
+    personsReview : () => {
+        
+    }
+
+}
